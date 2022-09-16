b2070263f7ee0cfbdbf9b215313129d577bff0dc
manirajigithub <109149403+manirajigithub@users.noreply.github.com>
2022-09-16 21:28:27 +0530

    Update EmployeePayRoll_JS_Day42.js

---
+++ EmployeePayRoll_JS_Day42.js
@@ -0,0 +1,32 @@
+// UC => 1 Check if Employee is Present or Not
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+    console.log("Employee is Absent");
+} else {
+    console.log("employee is Present");
+}
+
+// UC => 2 Calculating Daily Employee Wage
+// UC => 3 Refactor to Write in Function
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+empHrs=getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage is : " + empWage);
