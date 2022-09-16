fe6d1db9c0c1a5d6578bc21119debf27b736f9ef
manirajigithub <109149403+manirajigithub@users.noreply.github.com>
2022-09-16 21:29:23 +0530

    Update EmployeePayRoll_JS_Day42.js

---
+++ EmployeePayRoll_JS_Day42.js
@@ -0,0 +1,53 @@
+// UC => 1 Check if Employee is Present or Not
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+    console.log("Employee is Absent");
+} else {
+    console.log("employee is Present");
+}
+
+// UC => 2 Calculating Daily Employee Wage
+// UC => 3 Refactor to Write in Function
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+
+let empHrs = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+empHrs=getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage is : " + empWage);
+
+// UC => 4 Calculating Wages for a Month 
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hours: " + empHrs + " Employee Wage : " + empWage);
+
+// UC => 5 Calculate Wages till a Condition of total Working Hours or Working Days Reached for a Month
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days : " + totalWorkingDays + "  Total Hours : " + totalEmpHrs + "  Employee Wage : " + empWage);
